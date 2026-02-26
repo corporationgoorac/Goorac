32052d464f45b9eeed65dda094b3d2c46b3f5ee7
corporationgoorac <corporationgoorac@gmail.com>
2026-02-26 22:13:10 +0530

    Create keywords.js

---
+++ components/keywords.js
@@ -0,0 +1,11 @@
+// components/keywords.js
+window.effectKeywords = {
+    hearts: ['love', '❤️', 'meena', '🥰', '😍'],
+    confetti: ['congratulations', 'congrats', 'happy birthday', 'party', '🎉', 'yay'],
+    balloons: ['surprise', '🎈', 'celebrate', 'bday'],
+    stars: ['star', 'amazing', 'magic', '⭐', 'wow'],
+    snow: ['cold', 'snow', 'winter', 'freeze', '❄️', '⛄'],
+    fireworks: ['new year', 'boom', 'diwali', 'fireworks', '🎇', '🎆'],
+    thumbsup: ['good job', 'well done', 'awesome', '👍', 'great'],
+    rain: ['rain', 'sad', 'crying', 'storm', '🌧️', '☔']
+};
