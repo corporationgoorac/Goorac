b3743e021c94ce4d4373009d6b0681c20a275198
corporationgoorac <corporationgoorac@gmail.com>
2026-02-27 00:30:55 +0530

    Update keywords.js

--- components/keywords.js
+++ components/keywords.js
@@ -1,5 +1,6 @@
 // components/keywords.js
 window.effectKeywords = {
+    // Your original effects
     hearts: ['love', '❤️', 'meena', '🥰', '😍', 'sumaiya'],
     confetti: ['congratulations', 'congrats', 'happy birthday', 'party', '🎉', 'yay'],
     balloons: ['surprise', '🎈', 'celebrate', 'bday'],
@@ -7,5 +8,16 @@ window.effectKeywords = {
     snow: ['cold', 'snow', 'winter', 'freeze', '❄️', '⛄'],
     fireworks: ['new year', 'boom', 'diwali', 'fireworks', '🎇', '🎆'],
     thumbsup: ['good job', 'well done', 'awesome', '👍', 'great'],
-    rain: ['rain', 'sad', 'crying', 'storm', '🌧️', '☔']
+    rain: ['rain', 'sad', 'crying', 'storm', '🌧️', '☔'],
+    
+    // The newly added effects in the physics engine
+    money: ['money', 'cash', 'rich', 'pay', 'salary', '💵', '💰', '💸'],
+    bubbles: ['bubble', 'bubbles', 'pop', 'wash', 'clean', '🫧', '🛁'],
+    skulls: ['dead', 'skull', 'death', 'scary', 'spooky', '💀', '☠️'],
+    clovers: ['lucky', 'luck', 'clover', 'irish', '🍀', '☘️'],
+    clapping: ['clap', 'bravo', 'applause', 'brilliant', '👏', '🙌'],
+    poop: ['poop', 'crap', 'shit', 'bullshit', '💩'],
+    alien: ['alien', 'ufo', 'space', 'martian', '👽', '🛸'],
+    ghost: ['ghost', 'boo', 'haunted', 'phantom', '👻'],
+    leaves: ['autumn', 'fall', 'leaves', 'leaf', '🍂', '🍁']
 };
